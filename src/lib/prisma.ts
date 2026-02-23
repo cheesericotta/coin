530a678111ee133c6c26cc6e0502fda739d88c15
cheesericotta <hafiz99.budi@gmail.com>
2026-02-23 20:27:04 +0800

    huhu

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,21 @@
 import { PrismaClient } from "@prisma/client";
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+// Initialize prisma client
+let prisma: PrismaClient;
+
+if (typeof window === "undefined") {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalForPrisma.prisma;
+} else {
+  // Dummy client for client-side which shouldn't happen but needed to satisfy type
+  prisma = new PrismaClient();
+}
+
+export { prisma };
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
